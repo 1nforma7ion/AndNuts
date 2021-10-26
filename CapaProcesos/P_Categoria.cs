dde0cdadd0f4d099d0348621c2ee5c9278ab6ef2
Facundo Lenardon <37845416+1nforma7ion@users.noreply.github.com>
2021-10-26 12:24:44 -0300

    Add files via upload

---
+++ CapaProcesos/P_Categoria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CapaEntidades;
+using CapaDatos;
+
+namespace CapaProcesos
+{
+    public class P_Categoria
+    {
+        D_Categoria ObjDato = new D_Categoria();
+
+        public List<E_Categoria>ListandoCategoria(string buscar)
+        {
+            return ObjDato.ListarCategoria(buscar);
+        }
+
+        public void InsertandoCategoria(E_Categoria Categoria)
+        {
+            ObjDato.InsertarCategoria(Categoria);
+        }
+
+        public void EditandoCategoria(E_Categoria Categoria)
+        {
+            ObjDato.EditarCategoria(Categoria);
+        }
+
+        public void EliminandoCategoria(E_Categoria Categoria)
+        {
+            ObjDato.EliminarCategoria(Categoria);
+        }
+    }
+}
